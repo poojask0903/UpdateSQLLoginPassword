6aba7d6ef2541ce74eb1a8bcb7a392874e1f79f4
poojask0903 <poojask0903@gmail.com>
2016-06-14 16:20:08 +0000

    Add files via upload

---
+++ InsertData.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace UpdateSQLPassword
+{
+    public class InsertData
+    {
+        public static void InsertDBData(object[] obj, string str)
+        {
+            try
+            {
+                // Get Length of Object pass from View Page
+                int objectLength = obj.Length;
+
+                // Create Object to get store procedure parameters
+                object[] storedProcParameters = new object[objectLength];
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings[Constants.CONNECTION_STRING_KEY].ConnectionString.ToString()))
+                {
+                    // Check Connection Open or Close
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+
+                    // Start Code to get SQL parameter from Stored Procedure
+
+                    SqlCommand command = new SqlCommand();
+                    command.Connection = con;
+                    command.CommandText = str;
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    SqlCommandBuilder.DeriveParameters(command);
+
+                    for (int i = 0; i < command.Parameters.Count - 1; i++)
+                    {
+                        storedProcParameters[i] = command.Parameters[i + 1].ParameterName.ToString();
+                    }
+                    // End code to get SQL parameter from Stored Procedure
+                    // Start Code to Insert data into table using Stored Procedure
+                    using (SqlCommand cmd = new SqlCommand(str, con))
+                    {
+
+                        for (int i = 0; i < obj.Length; i++)
+                        {
+                            SqlParameter sp = new SqlParameter();
+                            sp.ParameterName = storedProcParameters[i].ToString();
+                            sp.Value = obj[i];
+                            cmd.Parameters.Add(sp);
+                        }
+
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                //End Code to Insert data into table using stored procedure
+            }
+
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ SendMail.cs
@@ -0,0 +1,76 @@
+﻿using RestSharp;
+using System;
+using System.Configuration;
+
+namespace UpdateSQLPassword
+{
+    class SendMail
+    {
+        public static IRestResponse SendEmail(string newPassword)
+        {
+            string MailBody = string.Format(Constants.MAIL_BODY, newPassword);
+            RestClient client = new RestClient();
+            client.BaseUrl = "https://api.mailgun.net/v2";
+            string apiKey = Convert.ToString(ConfigurationManager.AppSettings["APIKey"]);
+            if (!string.IsNullOrEmpty(apiKey))
+            {
+                client.Authenticator = new HttpBasicAuthenticator("api", apiKey);//Api Authentication
+            }
+            else
+            {
+                Console.WriteLine(Constants.MISSING_API_KEY_VALUE);
+            }
+            //Container for data used to make requests
+            RestRequest request = new RestRequest();
+            string domainName = Convert.ToString(ConfigurationManager.AppSettings["DomainName"]);
+            if (!string.IsNullOrEmpty(domainName))
+            {
+                request.AddParameter("domain", domainName, ParameterType.UrlSegment);
+            }
+            else
+            {
+                Console.WriteLine(Constants.MISSING_DOMAIN_KEY_VALUE);
+            }
+            request.Resource = "{domain}/messages";
+            string fromEmail = Convert.ToString(ConfigurationManager.AppSettings["FromEmail"]);
+            if (!string.IsNullOrEmpty(fromEmail))
+            {
+                request.AddParameter("from", fromEmail);
+            }
+            else
+            {
+                Console.WriteLine(Constants.MISSING_FROM_EMAIL_KEY_VALUE);
+            }
+            string toEmail = Convert.ToString(ConfigurationManager.AppSettings["ToEmail"]);
+            if (!string.IsNullOrEmpty(toEmail))
+            {
+            request.AddParameter("to", toEmail);
+            //request.AddParameter("to", toEmail);
+            }
+            else
+            {
+                Console.WriteLine(Constants.MISSING_TO_EMAIL_KEY_VALUE);
+            }
+            string ccEmail = Convert.ToString(ConfigurationManager.AppSettings["CcEmail"]);
+            if (!string.IsNullOrEmpty(ccEmail))
+            {
+            request.AddParameter("cc",ccEmail );
+            }
+            string subjectEmail = Convert.ToString(ConfigurationManager.AppSettings["SubjectEmail"]);
+            if (!string.IsNullOrEmpty(subjectEmail))
+            {
+                request.AddParameter("subject", subjectEmail);
+            }
+            else
+            {
+                Console.WriteLine(Constants.MISSING_SUBJECT_MESSAGE);
+            }
+            request.AddParameter("text", "");
+            request.AddParameter("html", MailBody);
+            request.Method = Method.POST;
+            var response = client.Execute(request);
+            return response;
+        }
+
+    }
+}
